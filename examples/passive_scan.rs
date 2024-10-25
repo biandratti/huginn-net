3505e891b8507c9045a16a9df07bc3ebcc2c4159
biandratti <maxibiandra@gmail.com>
2024-10-25 16:40:54 +0200

    db pub

--- examples/passive_scan.rs
+++ examples/passive_scan.rs
@@ -1,47 +1,22 @@
-use passivetcp::{Database, P0fOutput, SignatureDetails, SignatureMatcher};
-use pnet::datalink::{self, Config, Channel::Ethernet};
+use clap::Parser;
+use log::debug;
+use passivetcp::PassiveTcpFingerprinter;
+use passivetcp::db::Database;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    interface: String,
+}
 
 fn main() {
+    let args = Args::parse();
+    let interface_name = args.interface;
+
     let db = Database::default();
-    let matcher = SignatureMatcher::new(&db);
+    debug!("Loaded database: {:?}", db);
+    let fingerprinting_tool = PassiveTcpFingerprinter::new(&db);
 
-    let interface_name = "eth0";
-    let interfaces = datalink::interfaces();
-    let interface = interfaces
-        .into_iter()
-        .find(|iface| iface.name == interface_name)
-        .expect("Could not find the interface");
-
-    let config = Config {
-        promiscuous: true,
-        ..Config::default()
-    };
-
-    // Set up the datalink channel
-    let (_tx, mut rx) = match datalink::channel(&interface, config) {
-        Ok(Ethernet(tx, rx)) => (tx, rx),
-        Ok(_) => panic!("Unhandled channel type"),
-        Err(e) => panic!("Unable to create channel: {}", e),
-    };
-
-    // Listen for packets and print passive scan output
-    loop {
-        match rx.next() {
-            Ok(packet) => {
-                if let Ok(signature_details) = SignatureDetails::extract(packet) {
-                    if let Some((label, _)) = matcher.find_matching_signature(&signature_details.signature) {
-                        let output = P0fOutput {
-                            client: signature_details.client,
-                            server: signature_details.server,
-                            is_client: signature_details.is_client,
-                            label: Some(label.clone()),
-                            sig: signature_details.signature,
-                        };
-                        println!("{}", output);
-                    }
-                }
-            }
-            Err(e) => eprintln!("Failed to read packet: {}", e),
-        }
-    }
+    fingerprinting_tool.start_capture(&interface_name);
 }
---
+++ src/lib.rs
@@ -0,0 +1,77 @@
+// src/lib.rs
+
+pub mod db;
+mod display;
+mod http;
+mod p0f_output;
+mod packet;
+mod parse;
+mod signature_matcher;
+mod tcp;
+
+use crate::db::Database;
+use crate::p0f_output::P0fOutput;
+use crate::packet::SignatureDetails;
+use pnet::datalink::{self, Config, NetworkInterface};
+use crate::signature_matcher::SignatureMatcher;
+use log::debug;
+
+/// Main struct for the passive TCP fingerprinting library.
+pub struct PassiveTcpFingerprinter<'a> {
+    matcher: SignatureMatcher<'a>,
+}
+
+impl<'a> PassiveTcpFingerprinter<'a> {
+    // Constructor for PassiveTcpFingerprinter
+    pub fn new(database: &'a Database) -> Self {
+        let matcher = SignatureMatcher::new(database);
+        Self { matcher } // Assuming Database implements Clone
+    }
+
+    /// Starts capturing packets on the specified network interface.
+    pub fn start_capture(&self, interface_name: &str) {
+        let interfaces: Vec<NetworkInterface> = datalink::interfaces();
+        let interface: NetworkInterface = interfaces
+            .into_iter()
+            .find(|iface| iface.name == interface_name)
+            .expect("Could not find the interface");
+
+        let config = Config {
+            promiscuous: true,
+            ..Config::default()
+        };
+
+        let (_tx, mut rx) = match datalink::channel(&interface, config) {
+            Ok(datalink::Channel::Ethernet(tx, rx)) => (tx, rx),
+            Ok(_) => panic!("Unhandled channel type"),
+            Err(e) => panic!("Unable to create channel: {}", e),
+        };
+
+        loop {
+            match rx.next() {
+                Ok(packet) => {
+                    match SignatureDetails::extract(packet) {
+                        Ok(signature_details) => {
+                            if signature_details.signature.mss.is_some() {
+                                if let Some((label, _matched_signature)) =
+                                    self.matcher.find_matching_signature(&signature_details.signature)
+                                {
+                                    let p0f_output = P0fOutput {
+                                        client: signature_details.client,
+                                        server: signature_details.server,
+                                        is_client: signature_details.is_client,
+                                        label: Some(label.clone()),
+                                        sig: signature_details.signature,
+                                    };
+                                    println!("{}", p0f_output)
+                                }
+                            }
+                        }
+                        Err(e) => debug!("Failed to extract signature: {}", e),
+                    };
+                }
+                Err(e) => eprintln!("Failed to read: {}", e),
+            }
+        }
+    }
+}
