cc25a467af442797fc390f5f0ef58e2ebe7786d5
Biandratti <maxibiandra@gmail.com>
2024-10-26 15:54:02 +0200

    fmt code fixed

--- src/lib.rs
+++ src/lib.rs
@@ -24,17 +24,18 @@ impl<'a> P0f<'a> {
 
     pub fn analyze(&self, packet: &[u8]) -> Option<P0fOutput> {
         if let Ok(signature_details) = SignatureDetails::extract(packet) {
-                if let Some((label, _matched_signature)) =
-                    self.matcher.find_matching_signature(&signature_details.signature)
-                {
-                    return Some(P0fOutput {
-                        client: signature_details.client,
-                        server: signature_details.server,
-                        is_client: signature_details.is_client,
-                        label: Some(label.clone()),
-                        sig: signature_details.signature,
-                    });
-                }
+            if let Some((label, _matched_signature)) = self
+                .matcher
+                .find_matching_signature(&signature_details.signature)
+            {
+                return Some(P0fOutput {
+                    client: signature_details.client,
+                    server: signature_details.server,
+                    is_client: signature_details.is_client,
+                    label: Some(label.clone()),
+                    sig: signature_details.signature,
+                });
+            }
         }
         None
     }
