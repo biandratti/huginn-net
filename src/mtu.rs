eaeec3050a635c9c46ff7c68ce4a27f59c6e37e2
Biandratti <maxibiandra@gmail.com>
2024-11-01 14:13:45 +0100

    adding debug mode in the MTU

--- src/mtu.rs
+++ src/mtu.rs
@@ -1,3 +1,4 @@
+use log::debug;
 use pnet::packet::tcp::TcpFlags::SYN;
 use pnet::packet::tcp::TcpPacket;
 
@@ -7,10 +8,11 @@ fn is_client(tcp: &TcpPacket) -> bool {
 
 pub fn extract_from_ipv4(tcp: &TcpPacket, ipv4_header_len: u8, mss: u16) -> Option<u16> {
     if is_client(tcp) {
-        let tcp_header_len = (tcp.get_data_offset() * 4) as u16;
-        let ip_header_len = (ipv4_header_len * 4) as u16;
-        // MTU = MSS + IPv4 Header Length + TCP Header Length
-        Some(mss + ip_header_len + tcp_header_len)
+        let ip_header_len = (ipv4_header_len as u16) * 4; // convert to bytes
+        let tcp_header_len = (tcp.get_data_offset() as u16) * 4; // convert to bytes
+        let result = mss + ip_header_len + tcp_header_len;
+        debug!("MTU ipv4 {} - mss: {} - ip_header_len: {} - tcp_header_len: {}", result, mss, ip_header_len, tcp_header_len);
+        Some(result)
     } else {
         None
     }
@@ -18,10 +20,11 @@ pub fn extract_from_ipv4(tcp: &TcpPacket, ipv4_header_len: u8, mss: u16) -> Opti
 
 pub fn extract_from_ipv6(tcp: &TcpPacket, ipv6_header_len: u8, mss: u16) -> Option<u16> {
     if is_client(tcp) {
-        let tcp_header_len = (tcp.get_data_offset() * 4) as u16;
-
-        // MTU = MSS + IPv6 Header Length + TCP Header Length
-        Some(mss + ipv6_header_len as u16 + tcp_header_len)
+        let ip_header_len = (ipv6_header_len as u16) * 4; // convert to bytes
+        let tcp_header_len = (tcp.get_data_offset() as u16) * 4; // convert to bytes
+        let result = mss + ip_header_len + tcp_header_len;
+        debug!("MTU ipv6 {} - mss: {} - ip_header_len: {} - tcp_header_len: {}", result, mss, ip_header_len, tcp_header_len);
+        Some(result)
     } else {
         None
     }
