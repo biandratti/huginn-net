3a4aa91e20e49cd05ec014864cca231576e3e06f
Biandratti <maxibiandra@gmail.com>
2024-12-01 14:00:35 +0100

    mtu ipv6 fixed

--- src/mtu.rs
+++ src/mtu.rs
@@ -27,8 +27,12 @@ pub fn extract_from_ipv4(tcp: &TcpPacket, ipv4_header_len: u8, mss: u16) -> Opti
 
 pub fn extract_from_ipv6(tcp: &TcpPacket, ipv6_header_len: u8, mss: u16) -> Option<u16> {
     if from_client(tcp) {
-        let ip_header_len = (ipv6_header_len as u16) * 4; // convert to bytes
-        let tcp_header_len = (tcp.get_data_offset() as u16) * 4; // convert to bytes
+        let ip_header_len = ipv6_header_len as u16; // ipv6_header_len is in bytes already
+        let mut tcp_header_len = (tcp.get_data_offset() as u16) * 4; // convert to bytes
+        if tcp_header_len > 20 {
+            // If TCP header contains options
+            tcp_header_len -= 20;
+        }
         let mtu = mss + ip_header_len + tcp_header_len;
         debug!(
             "MTU ipv6 {} - mss: {} - ip_header_len: {} - tcp_header_len: {}",
