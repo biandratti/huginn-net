0c3142cc905c791349d8000e75552936a84bdc38
Biandratti <maxibiandra@gmail.com>
2024-12-14 15:21:40 +0100

    adding logger in the example

--- examples/p0f.rs
+++ examples/p0f.rs
@@ -1,5 +1,10 @@
 use clap::Parser;
 use log::{debug, info};
+use log4rs::append::console::ConsoleAppender;
+use log4rs::append::file::FileAppender;
+use log4rs::config::{Appender, Root};
+use log4rs::encode::pattern::PatternEncoder;
+use log4rs::Config;
 use passivetcp_rs::db::Database;
 use passivetcp_rs::P0f;
 use std::sync::mpsc;
@@ -8,15 +13,55 @@ use std::thread;
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    #[arg(short, long)]
+    #[arg(short = 'i', long)]
     interface: String,
+    #[arg(short = 'l', long = "log-file")]
+    log_file: Option<String>,
+}
+
+fn initialize_logging(log_file: Option<String>) {
+    let pattern = "{d} - {l} - {m}{n}"; // Format: timestamp - level - message
+    let console = ConsoleAppender::builder()
+        .encoder(Box::new(PatternEncoder::new(pattern)))
+        .build();
+
+    let config = if let Some(log_file) = log_file {
+        let file = FileAppender::builder()
+            .encoder(Box::new(PatternEncoder::new(pattern)))
+            .build(log_file)
+            .expect("Failed to create log file appender");
+
+        Config::builder()
+            .appender(Appender::builder().build("console", Box::new(console)))
+            .appender(Appender::builder().build("file", Box::new(file)))
+            .build(
+                Root::builder()
+                    .appender("console")
+                    .appender("file")
+                    .build(log::LevelFilter::Info),
+            )
+            .expect("Failed to build log4rs config")
+    } else {
+        Config::builder()
+            .appender(Appender::builder().build("console", Box::new(console)))
+            .build(
+                Root::builder()
+                    .appender("console")
+                    .build(log::LevelFilter::Info),
+            )
+            .expect("Failed to build log4rs config")
+    };
+
+    log4rs::init_config(config).expect("Failed to initialize log4rs");
 }
 
 fn main() {
-    env_logger::init();
+    //env_logger::init();
     let args = Args::parse();
 
     let db = Box::leak(Box::new(Database::default()));
+    initialize_logging(args.log_file.clone());
+
     debug!("Loaded database: {:?}", db);
 
     let (sender, receiver) = mpsc::channel();
